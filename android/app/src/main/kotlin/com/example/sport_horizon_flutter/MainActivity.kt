b6c3882445fc82197775a74a40cca89b61b39148
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-07-04 13:25:28 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sport_horizon_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sport_horizon_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
